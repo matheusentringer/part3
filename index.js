0bb87db08a98f5c3ed81131ef53116cbb5a8d2a4
Matheus <matheusentringer1@gmail.com>
2022-07-18 16:50:15 -0300

    3.6: Phonebook backend step6

--- index.js
+++ index.js
@@ -46,6 +46,18 @@ app.get('/api/persons', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if (!body.name || !body.number){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if (persons.find(person => body.name === person.name)){
+        return response.status(400).json({
+            error: 'repeated entry'
+        })
+    }
+
     const person = {
         id: Math.floor(Math.random() * 10000),
         name: body.name,
