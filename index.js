17aac9616480afd0935870f404e6bf54656b8f95
Matheus <matheusentringer1@gmail.com>
2022-07-19 14:29:25 -0300

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -86,6 +89,12 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
