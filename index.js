e19f59570b39aff7938be719d751a7f91dba0064
Matheus <matheusentringer1@gmail.com>
2022-07-20 10:55:52 -0300

    Testing backend on Heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { request, response } = require('express')
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
@@ -98,7 +101,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
