a3f345bd1b7ff627078cfb8fb416d39dad583488
Matheus <matheusentringer1@gmail.com>
2022-07-27 10:07:17 -0300

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -13,39 +13,19 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-        id: 1,
-        name: 'Arto Hellas', 
-        number: '040-123456'
-    },
-    { 
-        id: 2,
-        name: 'Ada Lovelace', 
-        number: '39-44-5323523'
-    },
-    { 
-        id: 3,
-        name: 'Dan Abramov', 
-        number: '12-43-234345'
-    },
-    { 
-        id: 4,
-        name: 'Mary Poppendieck', 
-        number: '39-23-6423122'
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
 
 app.get('/info', (request, response) => {
     let date = new Date()
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
+    Person.count({}).then(count => {
+      response.send(`
+        <p>Phonebook has info for ${count} people</p>
         <p>${date}</p>
-    `)
+      `)
+    })
+    
 })
 
 app.get('/api/persons', (request, response) => {
